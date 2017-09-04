f315d3111c1bf31a87b4e8a1b712bbfe0ca3bea8
Anandaroop Roy <anandaroop.roy+github@gmail.com>
2017-09-04 13:46:18 -0400

    Get port num from env

--- server.js
+++ server.js
@@ -8,6 +8,6 @@ app.get('/', (req, res) => {
   res.redirect('/artist-roster')
 })
 
-app.listen(3000, () => {
-  console.log('Listening on port 3000')
+app.listen(process.env.PORT, () => {
+  console.log(`Listening on port ${process.env.PORT}`)
 })
