8ac26a82879100e6eb604010d80854710e3b4080
Anandaroop Roy <anandaroop.roy+github@gmail.com>
2017-09-08 22:56:34 -0400

    Add automatic asset dir clobber

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
 
 module.exports = {
   entry: {
@@ -8,5 +9,8 @@ module.exports = {
     filename: '[name].js',
     path: path.resolve(__dirname, 'public/assets'),
     publicPath: '/assets'
-  }
+  },
+  plugins: [
+    new CleanWebpackPlugin(['public/assets'])
+  ]
 }
