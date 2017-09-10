39e7b338a30c2b051b7f83d170ee0ad4fdf4b232
Anandaroop Roy <anandaroop.roy+github@gmail.com>
2017-09-09 21:51:22 -0400

    Style a component

--- apps/artist-roster/components/App.js
+++ apps/artist-roster/components/App.js
@@ -1,8 +1,19 @@
 import React, { Component } from 'react'
+import styled from 'styled-components'
+
+const Heading = styled.h1`
+  color: red;
+  font-family: sans-serif;
+`
 
 class App extends Component {
   render() {
-    return <div>I'm a properly linted App!</div>
+    return (
+      <div>
+        <Heading>Hi Styles</Heading>
+        <p>I'm a properly linted App!</p>
+      </div>
+    )
   }
 }
 
