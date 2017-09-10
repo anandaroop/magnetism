75dcd0269680ccdef9a33835d7c9e00066f739cb
Anandaroop Roy <anandaroop.roy+github@gmail.com>
2017-09-09 22:50:11 -0400

    Add an enzyme wrapper snapshot

--- apps/artist-roster/components/App.test.js
+++ apps/artist-roster/components/App.test.js
@@ -1,19 +1,23 @@
 import React from 'react'
 import renderer from 'react-test-renderer'
 import { shallow } from 'enzyme'
+import toJson from 'enzyme-to-json'
+
 import App from './App'
 
 describe('App', () => {
   it('renders', () => {
-    const component = renderer.create(<App />).toJSON()
-    expect(component).toMatchSnapshot()
+    const component = renderer.create(<App />)
+    expect(component.toJSON()).toMatchSnapshot()
   })
 
   it('responds to clicks', () => {
     const component = shallow(<App />)
     expect(component.state('n')).toEqual(1)
-
+    expect(toJson(component)).toMatchSnapshot()
+    
     component.find('a').simulate('click', { preventDefault: jest.fn() })
     expect(component.state('n')).toEqual(2)
+    expect(toJson(component)).toMatchSnapshot()
   })
 })
