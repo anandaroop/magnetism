bd8f47edc94e47571f48cbdb3d2492ad87d42a44
Anandaroop Roy <anandaroop.roy+github@gmail.com>
2017-09-09 22:46:25 -0400

    Add some behavior and enzyme tests

--- apps/artist-roster/components/App.js
+++ apps/artist-roster/components/App.js
@@ -6,12 +6,50 @@ const Heading = styled.h1`
   font-family: sans-serif;
 `
 
+const List = styled.ul`
+  list-style: none;
+  margin: 0em;
+  padding: 0em;
+`
+const Item = styled.li`
+  display: inline-block;
+  color: red;
+  border: solid 1px red;
+  margin: 1em;
+  padding: 0.5em;
+`
+
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      n: 1
+    }
+    this.handleClick = this.handleClick.bind(this)
+  }
+
+  renderItems() {
+    return Array.from({ length: this.state.n }).map((v, i) => <Item key={i}>{i}</Item>)
+  }
+
+  handleClick(e) {
+    e.preventDefault()
+    this.setState({
+      n: this.state.n + 1
+    })
+  }
+
   render() {
     return (
       <div>
         <Heading>Hi Styles</Heading>
-        <p>I'm a properly linted App!</p>
+        <p>
+          Do you like numbers? Great!{' '}
+          <a href="#" onClick={this.handleClick}>
+            Add more
+          </a>
+        </p>
+        <List>{this.renderItems()}</List>
       </div>
     )
   }
--- apps/artist-roster/components/App.test.js
+++ apps/artist-roster/components/App.test.js
@@ -1,10 +1,19 @@
 import React from 'react'
 import renderer from 'react-test-renderer'
+import { shallow } from 'enzyme'
 import App from './App'
 
 describe('App', () => {
   it('renders', () => {
     const component = renderer.create(<App />).toJSON()
     expect(component).toMatchSnapshot()
+  })
+
+  it('responds to clicks', () => {
+    const component = shallow(<App />)
+    expect(component.state('n')).toEqual(1)
+
+    component.find('a').simulate('click', { preventDefault: jest.fn() })
+    expect(component.state('n')).toEqual(2)
   })
 })
