05a04689770f75838dee78063dafa39a07d9953f
Anandaroop Roy <anandaroop.roy+github@gmail.com>
2018-11-06 15:05:56 -0500

    Add clear-artists button

--- apps/artist-roster/components/Slugs/index.js
+++ apps/artist-roster/components/Slugs/index.js
@@ -3,23 +3,30 @@ import PropTypes from 'prop-types'
 import styled from 'styled-components'
 import { connect } from 'react-redux'
 
+import { removeAllArtists } from '../../actions'
+
 const propTypes = {
-  artistIds: PropTypes.array.isRequired
+  artistIds: PropTypes.array.isRequired,
+  onClick: PropTypes.func.isRequired
 }
 
 const slugString = artistIds => artistIds.join('; ')
 
-const Slugs = ({ artistIds }) => {
+const Slugs = ({ artistIds, onClick }) => {
   if (artistIds.length === 0) return null
   return (
     <Wrapper>
       <List>{slugString(artistIds)}</List>
       <Button
+        bg="purple"
         onClick={e => {
           navigator.clipboard.writeText(slugString(artistIds))
         }}
       >
         Copy to clipboard
+      </Button>
+      <Button bg="gray" p="0.5em" onClick={e => onClick()}>
+        Clear artists
       </Button>
     </Wrapper>
   )
@@ -30,16 +37,27 @@ const mapStateToProps = state => ({
   artistIds: state.artistIds
 })
 
-const ConnectedSlugs = connect(mapStateToProps)(Slugs)
+const mapDispatchToProps = dispatch => {
+  return {
+    onClick: () => {
+      dispatch(removeAllArtists())
+    }
+  }
+}
+
+const ConnectedSlugs = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Slugs)
 
 export { ConnectedSlugs as default, Slugs }
 
 const Wrapper = styled.div`
+  flex: 1 0 0;
   margin: 1em 0;
   padding: 1em 0;
   color: purple;
   display: flex;
-  /* justify-content: space-between; */
   align-items: stretch;
   height: 100px;
 `
@@ -52,11 +70,12 @@ const List = styled.div`
 `
 
 const Button = styled.button`
-  background: purple;
+  background: ${({ bg }) => bg || 'black'};
   color: white;
-  padding: 1em;
+  padding: ${({ p }) => p || '1em'};
   border: none;
   flex: 0 0 12em;
+  margin-right: 1em;
   cursor: pointer;
   &:active {
     background: black;
